31471173b67239348bd153be44f8485408980c2b
mona <mona.shakibapour@wunderbyte.at>
2024-04-18 13:04:48 +0200

    0.1.17

--- local/wunderbyte_table/version.php
+++ local/wunderbyte_table/version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wunderbyte_table';
-$plugin->release = '2.0.2';
-$plugin->version = 2024040200;
-$plugin->requires = 2020061500;
+$plugin->release = '2.0.4';
+$plugin->version = 2024041600;
+$plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
