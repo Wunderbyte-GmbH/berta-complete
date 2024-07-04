4b4ffdad538d2f3855aecf7baf1559c4dd2c5e67
mona <mona.shakibapour@wunderbyte.at>
2024-07-04 14:51:36 +0200

    0.3.2

--- mod/booking/classes/local/connectedcourse.php
+++ mod/booking/classes/local/connectedcourse.php
@@ -123,6 +123,8 @@ class connectedcourse {
      */
     private static function retrieve_categoryid(stdClass &$newoption, stdClass &$formdata) {
 
+        global $DB;
+
         $config = get_config('booking', 'newcoursecategorycfield');
         if (!empty($config) && $config !== "-1") {
             // FEATURE add more settingfields add customfield_ to ...
@@ -131,10 +133,19 @@ class connectedcourse {
             $category = new stdClass();
             $category->name = $formdata->{$cfforcategory};
 
-            if (!empty($category->name)) {
-                $categories = core_course_external::get_categories([
+            if (is_array($category->name)) {
+                $category->name = reset($categoryname);
+            }
+
+            if (is_string($category->name) && !empty($category->name)) {
+
+                try {
+                    $categories = core_course_external::get_categories([
                         ['key' => 'name', 'value' => $category->name],
-                ]);
+                    ]);
+                } catch (\Exception $e) {
+                    $categories = [];
+                }
 
                 if (empty($categories)) {
                     $category->idnumber = $category->name;
@@ -145,6 +156,11 @@ class connectedcourse {
                     $categoryid = $createdcats[0]['id'];
                 } else {
                     $categoryid = $categories[0]['id'];
+                }
+            } else if (is_int($category->name)) {
+                // We check if this is a valid category.
+                if ($DB->record_exists("course_categories", ['id' => $category->name])) {
+                    $categoryid = $category->name;
                 }
             }
         }
